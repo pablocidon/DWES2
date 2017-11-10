5e1a5498e3848c70235204e686932b9b03e68a6a
pablocidon <pablo.cidbar@gmail.com>
2017-11-10 13:46:32 +0100

    Ejercicio 6 Corregido

--- Tema4/practica6/practica6.php
+++ Tema4/practica6/practica6.php
@@ -7,41 +7,38 @@
 </head>
 <body>
 <?php
-include "../confUsuarios.php";
+include "../../confUsuarios.php";
 try{
     $miDB = new PDO(DATOSCONEXION, USER, PASSWORD);
     $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $filas=0;
-    $departamentos=array();
-    for($i=0;$i<3;$i++){
-        $departamentos[$i]=array(
-          'CodDepartamento'=>'',
-          'DescDepartamento'=>''
-        );
+    $miDB->beginTransaction();
+    $departamentos=array(
+        array(
+            'CodDepartamento'=>"IDM",
+            'DescDepartamento'=>"Idiomas"
+        ),array(
+            'CodDepartamento'=>"TEC",
+            'DescDepartamento'=>"Tecnologia"
+        ),array(
+            'CodDepartamento'=>"FOL",
+            'DescDepartamento'=>"Orientacion Laboral"
+        )
+    );
+    $consulta = $miDB->prepare("INSERT INTO Departamento (CodDepartamento,DescDepartamento) VALUES (:CodDepartamento,:DescDepartamento)");
+    $consulta->bindParam(":CodDepartamento",$codigo);
+    $consulta->bindParam(":DescDepartamento",$descripcion);
+    for($j=0;$j<count($departamentos);$j++){
+        $codigo=$departamentos[$j]['CodDepartamento'];
+        $descripcion=$departamentos[$j]['DescDepartamento'];
+        $consulta->execute();
     }
-    $departamentos[0]['CodDepartamento']="IDM";
-    $departamentos[0]['DescDepartamento']="Idiomas";
-    $departamentos[1]['CodDepartamento']="TEC";
-    $departamentos[1]['DescDepartamento']="Tecnologia";
-    $departamentos[2]['CodDepartamento']="FOL";
-    $departamentos[2]['DescDepartamento']="Orientacion Laboral";
-    $consulta = $miDB->exec("INSERT INTO Departamento (CodDepartamento,DescDepartamento) VALUES (:CodDepartamento,:DescDepartamento)");
-    for($j=0;$j<3;$j++){
-        $sentencia=$miDB->prepare($consulta);
-        $sentencia->bindParam(":CodDepartamento",$departamentos[$i]['CodDepartamento']);
-        $sentencia->bindParam(":DescDepartamento",$departamentos[$i]['DescDepartamento']);
-        $sentencia->execute();
-        $filas=$sentencia->rowCount();
-    }
-    if($filas==3){
-        echo "Se han realizado con éxito las inserciones";
-    }else{
-        echo "Error en las inserciones";
-    }
-    unset($miDB);
+    $miDB->commit();
+    echo "Se ha llevado a cabo la inserción";
 }catch (Exception $exception){
     echo "Error: ".$exception->getMessage().'<br>';
-    echo "Codigo de error" .$exception->getCode().'<br>';
+    echo "Codigo de error ".$exception->getCode().'<br>';
+}finally{
+    unset($consulta);
     unset($miDB);
 }
 ?>
