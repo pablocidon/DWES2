8e9b1ed7feb62d9998b5de2a9825d05c4d61d7c1
pablocidon <pablo.cidbar@gmail.com>
2017-11-10 12:26:37 +0100

    Fichero de configuración

---
+++ Tema4/confUsuarios.php
@@ -0,0 +1,7 @@
+<?php
+    define("HOST","localhost");
+    define("USER","DAW211");
+    define("PASSWORD","paso");
+    define("DATABASE","DAW211_DBdepartamentos");
+    define("DATOSCONEXION","mysql:host=localhost;dbname=DAW211_DBdepartamentos");
+?>
