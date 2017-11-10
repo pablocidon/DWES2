ef15c75d506bcac449e0a615a6e7af36b4a16d19
pablocidon <pablo.cidbar@gmail.com>
2017-11-10 13:54:29 +0100

    Ejercicio 7

---
+++ Tema4/practica7/codigo.php
@@ -0,0 +1,3 @@
+<?php
+show_source("practica7.php");
+?>
---
+++ Tema4/practica7/practica7.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Práctica 6</title>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="../../../../favicon.ico" type="image/x-icon">
+</head>
+<body>
+<?php
+if(file_exists("departamentos.xml")){
+
+}
+?>
+</body>
+</html>
