2e8ff98d3bfdeb1985591a3da13b7532db4e8aa7
pablocidon <pablo.cidbar@gmail.com>
2017-12-05 13:04:50 +0100

    Ejercicio de Array del examen

---
+++ Tema3/practica26/practica26.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Práctica 26</title>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="../../../favicon.ico" type="image/x-icon">
+</head>
+<body>
+<?php
+for($i=1;$i=365;$i++){
+    $provincias[$i] = array(
+        'Avila' => '',
+        'Burgos' => '',
+        'Leon' => '',
+        'Palencia' => '',
+        'Salamanca' => '',
+        'Segovia' => '',
+        'Soria' => '',
+        'Valladolid' => '',
+        'Zamora' => ''
+    );
+}
+?>
+</body>
+</html>
