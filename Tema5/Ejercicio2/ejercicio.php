167d578205dbc564468f88dd42f71d7708eb1922
pablocidon <pablo.cidbar@gmail.com>
2017-12-05 12:42:58 +0100

    Correcto de verdad

--- Tema5/Ejercicio2/ejercicio.php
+++ Tema5/Ejercicio2/ejercicio.php
@@ -5,21 +5,20 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {//Si el usuario no se ha identificado
     echo 'Has pulsado Cancelar';
     exit;
 } else {
-require 'confUsuarios.php';//Llamamos a la libreria
+require 'conexionDB.php';//Llamamos al fichero de configuración para conectarse a la base de datos
 $entrada = false;
 $usuario = $_SERVER['PHP_AUTH_USER'];//Alamacenamos el usuario en una variable.
 $password = hash('sha256', $_SERVER['PHP_AUTH_PW']); //Almacenamos la contraseña encriptandola.
 try {
     $miDB = new PDO(DATOSCONEXION, USER, PASSWORD);//Establecemos la conexion
     $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Llamamos a las excepciones
-    $consulta = $miDB->query("SELECT * FROM Usuarios WHERE CodUsuario=':usuario' AND Password=':password'");
+    /*Creamos la consulta preparada que nos indicará si el usuario introducido y la contraseña existen en la base de datos.*/
+    $consulta = $miDB->prepare("SELECT CodUsuario,Password FROM Usuarios WHERE CodUsuario=:usuario AND Password=:password");
+    //Parámetros de la consulta
     $consulta->bindParam(':usuario', $usuario);
     $consulta->bindParam(':password', $password);
-    $consulta->execute();
-    if ($consulta->rowCount() == 0) {
-        $entrada = true;
-    }
-    if ($entrada==true) {
+    $consulta->execute();//Ejecutamos la consulta
+    if ($consulta->rowCount() != 0) {//Si la consulta devuelve algún resultado
         session_start();//Iniciamos la sesión
         setcookie("ultimaConexion", date("d-m-Y g:i:s"), time() + 172800000);//Creamos la cookie
         if(isset($_COOKIE['ultimaConexion'])){//Si la cookie no ha expirado mostramos la fecha y hora de la última conexión
@@ -56,15 +55,15 @@ try {
         }else{
             echo "La variable '\$_COOKIE' está vacía";//En caso de que se encuentre vacía.
         }
-    }else {
+    }else {//En caso de que el usuario no esté autentificado
         echo "No puedes acceder al contenido";
     }
-} catch (PDOException $exception) {
+} catch (PDOException $exception) {//Si salta la excepción
     header('WWW-Authenticate: Basic realm="Pablo"');
     header('HTTP/1.0 401 Unauthorized');
     echo 'Error en el Login';
     exit;
-} finally {
+} finally {//Cerramos la conexión tanto si salta la excepción como si no
     unset($miDB);
     exit();
 }
