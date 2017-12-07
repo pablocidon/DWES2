824360af15e07d29f9581a66c3ac68bbe17f88c6
pablocidon <pablo.cidbar@gmail.com>
2017-12-07 17:08:25 +0100

    Corrección

--- Tema4/Encuesta/seguimiento.php
+++ Tema4/Encuesta/seguimiento.php
@@ -1,123 +1,131 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Seguimiento</title>
-    <link rel="shortcut icon" href="../../../favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="css/estilos.css">
-    <!--Archivo: seguimiento.php
-        Autor: Pablo Cidón.
-        Creado: 23/11/2017
-        Modificado: 27/11/2017 -->
-</head>
-<body>
 <?php
 include "librerias/confUsuarios.php";
-include "librerias/LibreriaValidacion.php";
-try{
-    $miDB = new PDO(DATOSCONEXION, USER, PASSWORD);//la conexión.
-    $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Llamamos a las excepciones
-    $fechaActual = date('d-m-Y H:i:s');//Declaramos una variable para almacenar la fecha y hora actuales
-    /*
-     * Creamos la consulta que nos va a devolver la edad partiendo de la fecha de nacimiento, el número de participantes, la media del grado de
-     * satrisfacción y el número de equipos desde los que se han realizado las encuestas
-     */
-    $consulta1="SELECT AVG(TIMESTAMPDIFF(YEAR, FechaNacimiento, CURDATE())) AS EdadMedia ,COUNT(*) AS NumeroParticipantes ,AVG(Satisfaccion) AS MediaSatisfaccion, COUNT(DISTINCT IP) AS NumeroEquipos FROM Campos";
-    $sentencia1=$miDB->preparare($consulta1);// Preparamos la consulta
-try {// Ejecutamos la consulta
-    $sentencia1->execute();
-} catch (PDOException $PdoE) {//Si ha habido un error mostrar el mensaje de error y cerramos la conexion
-    ?>
-    <script>alert(<?php echo "No se ha podido ejecutar la consulta 1";?>)</script>
-<?php
-unset($miDB);
-}
-$resultado1=$sentencia1->fetch(PDO::FETCH_OBJ );// Guardamos el resultado como un objeto
-/*
- * Creamos la consulta que nos va a devolver todos aquellos equipos que han realizado encuestas, y el número de las mismas
- */
-$consulta2 = "SELECT IP,COUNT(IP) AS NumeroEncuestas FROM Campos GROUP BY IP HAVING NumeroEncuestas > 1";
-$sentencia2=$miDB->preparare($consulta2);// Preparamos la consulta
-try {// Ejecutamos la consulta
-    $sentencia2->execute();
-} catch (PDOException $PdoE) {//Si ha habido un error mostrar el mensaje de error y cerramos la conexion
-?>
-    <script>alert(<?php echo "No se ha podido ejecutar la consulta 2";?>)</script>
-<?php
-unset($miDB);
-}
-/*
- * Consulta que nos va a devolver el nombre y apellidos de los alumnos los cuales han mostrado su opinión
- */
-$consulta3 = "SELECT Nombre,Apellidos,Opinion FROM Campos WHERE Opinion != ''";
-$sentencia3=$miDB->preparare($consulta3);// Preparamos la consulta
-try {// Ejecutamos la consulta
-    $sentencia3->execute();
-} catch (PDOException $PdoE) {//Si ha habido un error mostrar el mensaje de error y cerramos la conexion
-?>
-    <script>alert(<?php echo "No se ha podido ejecutar la consulta 3";?>)</script>
-<?php
-unset($miDB);
+try {
+    //Creamos la conexion a la base de datos
+//Creamos la conexion a la base de datos
+    $db = new PDO(DATOSCONEXION, USER, PASSWORD);
+    //Definición de los atributos para lanzar una excepcion si se produce un error
+//Definición de los atributos para lanzar una excepcion si se produce un error
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $PdoE) {
+    //Capturamos la excepcion en caso de que se produzca un error,mostramos el mensaje de error y deshacemos la conexion
+//Capturamos la excepcion en caso de que se produzca un error,mostramos el mensaje de error y deshacemos la conexion
+    echo($PdoE->getMessage());
+    unset($db);
 }
 
-}catch (PDOException $exception){//Si se produce un error muestra un mensaje
-?>
-    <script>alert(<?php  echo $e->getMessage();?>)</script>
-    <?php
-}finally{//Finalmente cerramos la conexión
-    unset($miDB);
-    exit();
+//Generamos la fecha actual
+$fecha = date('d-m-Y H:i:s');
+//Creamos la consulta, esta consulta nos devolverá
+//- Media de edad
+//- Numero de encuestas realizadas
+//- Media de grado satisfaccion
+//- Media de horas de estudio diarias
+//- Numero de equipos distintos desde el que se ha realizado la encuesta
+$consulta1 = "SELECT AVG(TIMESTAMPDIFF(YEAR, FechaNacimiento, CURDATE())) AS EdadMedia ,COUNT(*) AS NumeroParticipantes ,AVG(Satisfaccion) AS MediaSatisfaccion, COUNT(DISTINCT IP) AS NumeroEquipos FROM Campos";
+//Preparamos la consulta
+$sentencia1 = $db->prepare($consulta1);
+$sentencia1->execute();
+//Ejecutamos la consulta
+try {
+    $sentencia1->execute();
+} catch (PDOException $PdoE) {
+    //Si ha habido un error mostrar el mensaje de error y cerramos la conexion
+    echo "Error al ejecutar la consulta 1";
+    unset($db);
+}
+
+//Guardamos el resultado como un objeto
+$resultadoConsulta1 = $sentencia1->fetch(PDO::FETCH_OBJ);
+
+//Creamos la segunda consulta, esta consulta nos devolverá
+//- IP y numero de encuestas realizadas desde esa IP si se ha realizado mas de una encuesta desde esa IP
+$consulta2 = "SELECT IP,COUNT(IP) AS NumeroEncuestas FROM Campos GROUP BY IP HAVING NumeroEncuestas > 1";
+//Preparamos la consulta
+$sentencia2 = $db->prepare($consulta2);
+$sentencia2->execute();
+//Ejecutamos la consulta
+try {
+    $sentencia2->execute();
+} catch (PDOException $PdoE) {
+    //Si ha habido un error mostrar el mensaje de error y cerramos la conexion
+    echo "Error al ejecutar la consulta 2";
+    unset($db);
+}
+
+//Creamos la tercera consulta, esta consulta nos devolverá
+//- Nombre y Apellidos del encuesta y su opinion siempre que no sea vacia
+$consulta3 = "SELECT Nombre,Apellidos,Opinion FROM Campos WHERE Opinion != ''";
+//Preparamos la consulta
+$sentencia3 = $db->prepare($consulta3);
+$sentencia3->execute();
+//Ejecutamos
+try {
+    $sentencia3->execute();
+} catch (PDOException $PdoE) {
+    //Si ha habido un error mostrar el mensaje de error y cerramos la conexion
+    echo "Error al ejecutar la consulta 3";
+    unset($db);
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Encuesta</title>
+    <link rel="shortcut icon" href="../../../favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <!--Archivo: encuesta.php
+        Autor: Pablo Cidón.
+        Creado: 23/11/2017
+        Modificado: 07/12/2017 -->
+</head>
+<body>
 <div>
-    <h1>Seguimiento de los Resultados de las Encuestas</h1>
+    <h2>RESULTADOS DE LA ENCUESTA</h2>
     <?php
-    echo("<strong>Fecha y hora actuales:</strong>     $fechaActual<br><br>");//Mostramos la fecha y hora actuales
-    echo("<strong>Número de Participantes:</strong> $resultado1->NumeroParticipantes<br><br>");//Mostramos el número de participantes
-    echo("<strong>Edad Media:</strong>" .round($resultado1->EdadMedia, 2, PHP_ROUND_HALF_UP)."<br><br>");//Mostramos la media de edad redondeando
-    echo("<strong>Media Grado de Satisfacción:</strong>" .round($resultado1->MediaSatisfaccion, 2, PHP_ROUND_HALF_UP)."<br><br>");//Mostramos la media redondeando del grado de satisfacción
-    echo("<strong>Numero de Equipos:</strong> $resultado1->NumeroEquipos<br><br>");//Mostramos el número de equipos desde los que se han realizado las encuestas
-    echo '<hr>';
-    echo("<strong>Dirección IP desde la que se han realizado más de 1 encuesta:</strong><br><br>");
+    //Mostramos la fecha y hora
+    echo("<strong>Fecha y hora actuales:</strong>       $fecha<br /><br />");
+
+    //Mostramos el numero de participantes
+    echo("<strong>Número de alumnos participantes:</strong> $resultadoConsulta1->NumeroParticipantes<br /><br />");
+    //Mostramos la edad promedio
+    echo("<strong>Edad promedio:</strong> ".round($resultadoConsulta1->EdadMedia,2,PHP_ROUND_HALF_UP)."<br /><br />");
+    //Mostramos el grado de satisfaccion promedio
+    echo("<strong>Promedio de grado de satisfacción:</strong> " . round($resultadoConsulta1->MediaSatisfaccion, 2, PHP_ROUND_HALF_UP) . "<br /><br />");
+    //Mostramos el numero de equipos distintos desde el que sea ha realizado la encuesta
+    echo("<strong>Número de equipos desde los que se ha realizado la encuesta:</strong>         $resultadoConsulta1->NumeroEquipos<br /><br />");
+
+    //Devolvemos los resultados de la consulta2, recogemos las filas como un objeto y avanzamos el puntero
+    echo "<strong>Dirección IP de los equipos desde los que se ha realizado la encuesta más de una vez:</strong><br /><br />";
+    echo "<table border='1'><tr><th>Direccion IP</th><th>Numero de veces que ha realizado la encuesta</th></tr>";
+    while ($resultadoConsulta2 = $sentencia2->fetch(PDO::FETCH_OBJ)) {
+        echo "<tr>";
+        echo "<td>$resultadoConsulta2->IP</td>";
+        echo "<td>$resultadoConsulta2->NumeroEncuestas</td>";
+        echo "</tr>";
+    }
+    echo "</table><br /><br />";
+
+    //Devolvemos los resultados de la consulta3, recogemos las filas como un objeto y avanzamos el puntero
+    echo "<strong>Listado de opiniones y sugerencias recibidas:</strong><br /><br />";
+    echo "<table border='1'><tr><th>Alumno</th><th>Opiniones</th></tr>";
+    while ($resultadoConsulta3 = $sentencia3->fetch(PDO::FETCH_OBJ)) {
+        echo "<tr>";
+        echo "<td>$resultadoConsulta3->Nombre $resultadoConsulta3->Apellidos </td>";
+        echo "<td>$resultadoConsulta3->Opinion</td>";
+        echo "</tr>";
+    }
+    echo "</table><br /><br />";
+
     ?>
-    <table border="1">
-        <tbody>
-        <th>
-        <td>Dirección IP</td>
-        <td>Número de encuestas</td>
-        </th>
-        <?php
-        while ($resultado2 = $sentencia2->fetch(PDO::FETCH_OBJ)) {
-            echo "<tr>";
-            echo "<td>$resultado2->IP</td>";
-            echo "<td>$resultado2->NumeroEncuestas</td>";
-            echo "</tr>";
-        }
-        ?>
-        </tbody>
-    </table><hr>
-    <?php
-    echo("<strong>Listado de opiniones y sugerencias:</strong><br><br>");
-    ?>
-    <table border="1">
-        <tbody>
-        <th>
-        <td>Alumnos</td>
-        <td>Opinión</td>
-        </th>
-        <?php
-        while ($resultado3 = $sentencia3->fetch(PDO::FETCH_OBJ)) {
-            echo "<tr>";
-            echo "<td>$resultado3->Apellidos $resultado3->Nombre</td>";
-            echo "<td>$resultado3->Opinion</td>";
-            echo "</tr>";
-        }
-        ?>
-        </tbody>
-    </table><hr>
-    <input type="button" onclick="location.href = 'index.html'" name="Volver" value="Volver">
-    <?php
-    ?>
+    <input type="button" onclick="location.href='index.html'" name="Volver" value="Volver">
 </div>
+<?php
+unset($db);
+?>
+<footer>
+    <p id="pie">Pablo Cidon Curso 2017-2018</p>
+</footer>
 </body>
 </html>
