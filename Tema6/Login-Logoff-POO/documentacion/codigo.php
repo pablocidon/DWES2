2a505f6f415f651622b570d0fd79b90fcf198bd6
pablocidon <pablo.cidbar@gmail.com>
2018-01-15 13:07:35 +0100

    Codigo fuente de la aplicación

---
+++ Tema6/Login-Logoff-POO/documentacion/codigo.php
@@ -0,0 +1,62 @@
+<?php
+/*
+    * Autor: Pablo Cidón.
+    * Creado: 15-01-2018.
+    * Archivo: codigo.php
+    * Modificado:
+*/
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" type="text/css" href="../base.css"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="shortcut icon" href="../../../../favicon.ico" type="image/x-icon">
+    <title>PCB-DWES</title>
+</head>
+<body>
+<h1>DBPDO.php</h1>
+<?php
+show_source("../datos/DBPDO.php");
+?>
+<h1>UsuarioDB.php</h1>
+<?php
+show_source("../datos/UsuarioDB.php");
+?>
+<h1>UsuarioPDO.php</h1>
+<?php
+show_source("../datos/UsuarioPDO.php");
+?>
+<h1>cinicio.php</h1>
+<?php
+show_source("../controlador/cinicio.php");
+?>
+<h1>clogin.php</h1>
+<?php
+show_source("../controlador/clogin.php");
+?>
+<h1>index.php</h1>
+<?php
+show_source("../controlador/index.php");
+?>
+<h1>Usuario.php</h1>
+<?php
+show_source("../modelo/Usuario.php");
+?>
+<h1>layout.php</h1>
+<?php
+show_source("../vista/layout.php");
+?>
+<h1>vinicio.php</h1>
+<?php
+show_source("../vista/vinicio.php");
+?>
+<h1>vlogin.php</h1>
+<?php
+show_source("../vista/vlogin.php");
+?>
+<footer>
+    <p id="pie">Pablo Cidon Curso 2017-2018</p>
+</footer>
+</body>
+</html>
