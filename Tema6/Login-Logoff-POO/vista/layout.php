e2b34b9bb2d21640c089bfb7a678e0a22c4c543d
pablocidon <pablo.cidbar@gmail.com>
2018-01-18 13:06:28 +0100

    añadir layout

--- Tema6/Login-Logoff-POO/vista/layout.php
+++ Tema6/Login-Logoff-POO/vista/layout.php
@@ -3,7 +3,31 @@
     * Autor: Pablo Cidón.
     * Creado: 12-01-2018.
     * Archivo: layout.php
-    * Modificado: 12-01-2018.
+    * Modificado: 18-01-2018.
 */
-
+$mostrar = 'vista/vlogin.php';
+if(isset($_GET['pagina'])){
+    $mostrar = $vistas[$_GET['pagina']];
+}
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" type="text/css" href="../base.css"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="shortcut icon" href="../../../../favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="webroot/css/estilos.css">
+    <title>PCB-DWES</title>
+</head>
+<body>
+<header>
+    <h1>LOGIN-LOGOFF</h1>
+</header>
+<form name="input" action="<?PHP echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <?php require_once $mostrar ?>
+</form>
+<footer>
+    <p id="pie">Pablo Cidon Curso 2017-2018</p>
+</footer>
+</body>
+</html>
--- Tema6/Login-Logoff-POO/vista/vlogin.php
+++ Tema6/Login-Logoff-POO/vista/vlogin.php
@@ -3,24 +3,10 @@
     * Autor: Pablo Cidón.
     * Creado: 12-01-2018.
     * Archivo: vlogin.php
-    * Modificado: 17-01-2018.
+    * Modificado: 18-01-2018.
 */
 
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <link rel="stylesheet" type="text/css" href="../base.css"/>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link rel="shortcut icon" href="../../../../favicon.ico" type="image/x-icon">
-    <!--<link rel="stylesheet" type="text/css" href="css/estilos.css">-->
-    <title>PCB-DWES</title>
-</head>
-<body>
-<header>
-    <h1>LOGIN-LOGOFF</h1>
-</header>
-<form name="input" action="<?PHP echo $_SERVER['PHP_SELF']; ?>" method="post">
     <label for="usuario">Usuario</label>
     <input type="text" placeholder="Usuario" name="usuario">
     <br><br>
@@ -31,9 +17,4 @@
     <br><br>
     <input type="submit" value="Aceptar" name="aceptar">
     <input type="submit" value="Cancelar" name="cancelar">
-</form>
-<footer>
-    <p id="pie">Pablo Cidon Curso 2017-2018</p>
-</footer>
-</body>
-</html>
+
