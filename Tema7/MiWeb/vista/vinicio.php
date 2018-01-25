a3eb457b1fcb51ccaa0e00e18adcad0440e1ca52
pablocidon <pablo.cidbar@gmail.com>
2018-01-25 16:11:04 +0100

    Estilos CSS-Bootstrap (2)

--- Tema7/MiWeb/vista/vinicio.php
+++ Tema7/MiWeb/vista/vinicio.php
@@ -6,10 +6,12 @@
     * Modificado: 19-01-2018.
 */
 ?>
+    <div class="color1">
+        <?php echo "<strong>Bienvenido </strong>.<br>";
+            print_r($_SESSION);
+        ?>
+    </div>
+    <br>
+    <input type="submit" name="salir" value="Cerrar Sesión" class="btn btn-primary" style="margin-bottom: 2%">
 
-    <?php echo "<strong>Bienvenido </strong>.<br>";
-        print_r($_SESSION);
-    ?>
-
-    <input type="submit" name="salir" value="Cerrar Sesión">
 
--- Tema7/MiWeb/vista/vlogin.php
+++ Tema7/MiWeb/vista/vlogin.php
@@ -7,30 +7,66 @@
 */
 
 ?>
-    <label for="usuario">Usuario</label>
-    <input type="text" placeholder="Usuario" name="usuario">
-    <br><br>
-    <label for="password">Contraseña</label>
-    <input type="password" placeholder="Contraseña" name="password">
-    <br><br>
-    <?php echo "<span class='error'>".$error."</span>";?>
-    <br><br>
-    <input type="submit" value="Aceptar" name="aceptar">
-    <input type="submit" value="Cancelar" name="cancelar">
-    <br><br>
-    No tienes cuenta? <input type="submit" name="registro" value="Registrate">
-    <br><br>
-    <h3>Catálogo de Requisitos:</h3>
+    <div class="form-group">
+        <label class="control-label col-md-2" for="usuario">Usuario</label>
+        <div class="col-md-10">
+            <input type="text" class="form-control" placeholder="Usuario" name="usuario">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-md-2" for="password">Contraseña</label>
+        <div class="col-md-10">
+            <input type="password" class="form-control" placeholder="Contraseña" name="password">
+        </div>
+    </div>
+    <div class="col-md-offset-2">
+        <?php echo "<span class='text-warming'>".$error."</span>";?>
+    </div>
+    <div class="form-group">
+        <div class="col-md-2 col-md-offset-5">
+            <input type="submit" value="Aceptar" name="aceptar" class="btn btn-success">
+            <input type="submit" value="Cancelar" name="cancelar" class="btn btn-danger">
+        </div>
+    </div>
+    <div class="col-md-offset-5">
+        No tienes cuenta? <input type="submit" name="registro" value="Registrate" class="btn btn-primary">
+    </div>
 
+    <br><br>
+    <div class="color1 row">
+        <h3>Catálogo de Requisitos:</h3>
+    </div>
+
+<div class="color1 row">
     <h3>Diagrama de Casos de Uso:</h3>
+</div>
 
+<div class="color1 row">
     <h3>Diagrama de Clases:</h3>
-    <img src="documentacion/diagramaClases.JPG" alt="Diagrama de Clases">
+    <img src="documentacion/diagramaClases.JPG" alt="Diagrama de Clases" style="width: 60%">
+</div>
+
+<div class="color1 row">
     <h3>Árbol de Navegación:</h3>
     <img src="documentacion/navegacion.JPG" alt="Árbol de Navegación">
-    <h3>Modelo Físico de Datos:</h3>
+</div>
 
+<div class="color1 row">
+    <h3>Modelo Físico de Datos:</h3>
+</div>
+
+<div class="color1 row">
     <h3>Árbol de Almacenamiento:</h3>
     <img src="documentacion/almacenamiento.JPG" alt="Árbol de Almacenamiento">
+</div>
+
+
+
+
+
+
+
+
+
 
 
