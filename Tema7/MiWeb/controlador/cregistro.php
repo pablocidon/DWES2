81ccd0c21bff5bb6e9b79d298c6163f5ff55b137
pablocidon <pablo.cidbar@gmail.com>
2018-01-31 12:20:40 +0100

    Controlador del registro

--- Tema7/MiWeb/controlador/cregistro.php
+++ Tema7/MiWeb/controlador/cregistro.php
@@ -7,22 +7,41 @@
 */
 $vista='registro';
 $correcto = false;
-$mensajeError = '';
-if(isset($_POST['cancelar'])){
-    header('Location: index.php?pagina=login');
-}
-if(isset($_SESSION['usuario'])){
-    header("Location: index.php?pagina=registro");
-}else{
-    $_GET["pagina"]="registro";
-    require_once 'vista/layout.php';
-}
+$mensajeError=Array(
+    'codUsuario'=>'',
+    'descUsuario'=>'',
+    'password'=>'',
+    'password2'=>'',
+    'distinto'=>''
+);
+
 if(isset($_POST['aceptar'])){
-    if($_POST['password'] === $_POST['password2']){
-        $correcto = true;
-    }else{
-        $correcto = false;
-        $mensajeError='Las contraseñas no coinciden';
+    if($mensajeError['codUsuario'] = comprobarAlfaNumerico($_POST['codUsuario'],10,3,1) == null){
+        $comprobacion = Usuario::consultaPerfil($_POST['codUsuario']);
+        $resultado = $comprobacion->fetchColumn(0);
+        if ($resultado) {//En caso de exista mostraremos un mensaje de error.
+            $mensajeError['codUsuario'] = "Este código ya existe";
+            $correcto = false;
+        }
+    }
+    $mensajeError['descUsuario'] = comprobarTexto($_POST['descUsuario'],100,1,1);
+    if($mensajeError['password'] = comprobarAlfaNumerico($_POST['password'],100,2,1) == null && $mensajeError['password2'] = comprobarAlfaNumerico($_POST['password2'],100,2,1) == null){
+        if($_POST['password']===$_POST['password2']){
+            $correcto = true;
+        }else{
+            $mensajeError['distinto'] = 'Las contraseñas no coinciden';
+        }
+    }
+    foreach ($errores as $valor) {  //recorremos el array de errores
+        if ($valor != null) {
+            $correcto = false;
+        }
     }
 }
+if(isset($_POST['cancelar'])){
+    header('Location: index.php?pagina=login');
+}else{
+    require_once 'vista/layout.php';
+}
+
 ?>
